ff653ebf749dd360a8c6c535d1535df8978bbfe6
luojunbang <luojunbang@foxmail.com>
2022-08-11 10:02:01 +0800

    change to monorepo

---
+++ packages/utils/test/mysql.js
@@ -0,0 +1,23 @@
+// const mysql = require('../src/mysql/client')
+
+// const DB_CONFIG = {
+//   host: 'localhost',
+//   user: 'lo',
+//   password: '0220',
+//   database: 'common_serve',
+// }
+
+// async function init_mysql() {
+//   const { pool } = await mysql(DB_CONFIG)
+//   pool.getConnection()
+//   pool
+//     .query('SELECT * FROM my_bill')
+//     .then((res) => {
+//         console.log(res)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// }
+
+// init_mysql()
