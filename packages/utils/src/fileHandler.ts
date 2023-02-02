669a57974a291c6d8ec3d99c590478153833eb50
luojunbang <luojunbang@foxmail.com>
2023-02-02 15:12:09 +0800

    feat: add dataURLtoFile

--- packages/utils/src/fileHandler.ts
+++ packages/utils/src/fileHandler.ts
@@ -45,3 +45,15 @@ export function generatorFileAxios(res: AxiosResponse, fileName?: string, type?:
   const { headers, data } = res
   generatorFile(fileName ?? parseFileName(headers['content-disposition']), data, type)
 }
+
+export function dataURLtoFile(dataurl: string, filename: string) {
+  const arr = dataurl.split(',')
+  let mime = (arr[0].match(/:(.*?);/) ?? [])[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new File([u8arr], filename, { type: mime })
+}
