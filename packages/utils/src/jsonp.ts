cc25862aac62a4ef90932314cd0185f0f9c26911
luojunbang <luojunbang@foxmail.com>
2023-10-16 11:30:59 +0800

    fix: 修改window typescript 定义

--- packages/utils/src/jsonp.ts
+++ packages/utils/src/jsonp.ts
@@ -9,21 +9,25 @@ import { parseParams, r } from './index'
  */
 export function jsonp<T>(url: string, params: Record<string, any> = {}): Promise<{ data: T }> {
   if (!window) return Promise.reject()
-
+  const _w = window.top as Window
   return new Promise((rs, rj) => {
-    const _id: string = 'jsonp_' + r()
-    Reflect.defineProperty(window, _id, {
-      writable: false,
-      configurable: true,
-      enumerable: false,
-      value: function (res: T) {
-        rs({ data: res })
-        Reflect.deleteProperty(window, _id)
-      },
-    })
-    const script = document.createElement('script')
-    const _params = parseParams(params)
-    script.src = url + _id + (_params ? '&' : '') + _params
-    document.head.append(script)
+    try {
+      const _id: string = 'jsonp_' + r()
+      Reflect.defineProperty(_w, _id, {
+        writable: false,
+        configurable: true,
+        enumerable: false,
+        value: function (res: T) {
+          rs({ data: res })
+          Reflect.deleteProperty(window, _id)
+        },
+      })
+      const script = _w.document.createElement('script')
+      const _params = parseParams(params)
+      script.src = url + _id + (_params ? '&' : '') + _params
+      _w.document.head.append(script)
+    } catch (e) {
+      rj(e)
+    }
   })
 }
