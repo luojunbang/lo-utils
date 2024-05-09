efc182518c183ae4b616ce89d9dfc1c6e0cf5be3
luojunbang <luojunbang@foxmail.com>
2024-05-09 17:48:35 +0800

    release: test github action2

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$PACKAGE_NAME" ]; then
+    PACKAGE_NAME="utils"
+fi
+
+echo $TOKEN
+
+echo "\n Current release package is $PACKAGE_NAME \n"
+
+last_version=$(git tag -l "$PACKAGE_NAME/*" | sort -rV | head -n 1)
+
+major_minor_version=${last_version%.*}
+revision_number=${last_version##*.}
+
+new_revision_number=$((revision_number+1))
+
+next_version="${major_minor_version}.${new_revision_number}"
+
+echo "\n Generate next version $next_version \n"
+
+git tag "$next_version"
+
+git push origin "$next_version"
+
