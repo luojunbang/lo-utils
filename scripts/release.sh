f45db3bedb33e00ed1ef76ef30b296114b258504
luojunbang <luojunbang@foxmail.com>
2024-05-10 15:54:37 +0800

    feat: no release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+set -e
 
 if [ -z "$PACKAGE_NAME" ]; then
     PACKAGE_NAME="utils"
 fi
 
-echo $TOKEN
+echo "Token: $GITHUB_REPOSITORY"
 
 echo "\n Current release package is $PACKAGE_NAME \n"
+
+git fetch --tags
 
 last_version=$(git tag -l "$PACKAGE_NAME/*" | sort -rV | head -n 1)
 
 major_minor_version=${last_version%.*}
 revision_number=${last_version##*.}
 
-new_revision_number=$((revision_number+1))
+new_revision_number=$((revision_number + 1))
 
 next_version="${major_minor_version}.${new_revision_number}"
 
@@ -21,5 +24,4 @@ echo "\n Generate next version $next_version \n"
 
 git tag "$next_version"
 
-git push origin "$next_version"
-
+git push "https://$GITHUB_TOKEN@github.com/luojunbang/lo-utils.git" "$next_version"
